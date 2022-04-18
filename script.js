d034cbf1aa55bb16642d9e55f58eee9a4f0bc311
Rafael Guzman <rega1237@gmail.com>
2022-04-18 17:10:13 -0400

    Add functionality remove book

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const booksContainer = document.querySelector('.books-container');
+const bookContainer = document.querySelector('.books-container');
 const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const addBookBtn = document.querySelector('.add-book-btn');
+const removeBtns = document.querySelectorAll('.remove');
 
 let booksArr = [];
+
+if(localStorage.getItem('books')) {
+    booksArr = JSON.parse(localStorage.getItem('books'));
+}
 
 class Book  {
     constructor(title, author) {
@@ -13,26 +18,38 @@ class Book  {
 };
 
 const render = () => {
-    booksContainer.innerHTML = null;
+    bookContainer.innerHTML = null;
     if(localStorage.getItem('books')) {
         booksArr = JSON.parse(localStorage.getItem('books'));
     }
     for(let i = 0; i <booksArr.length; i += 1) {
+        const bookDiv = document.createElement('div')
         const bookTitle = document.createElement('p');
         const bookAuthor = document.createElement('p');
         const removeBookBtn = document.createElement('button');
         const lineBreak = document.createElement('hr');
+        removeBookBtn.className = 'remove';
         bookTitle.textContent = booksArr[i].title;
         bookAuthor.textContent = booksArr[i].author;
         removeBookBtn.textContent = 'Remove';
-        booksContainer.appendChild(bookTitle);
-        booksContainer.appendChild(bookAuthor);
-        booksContainer.appendChild(removeBookBtn);
-        booksContainer.appendChild(lineBreak);
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(removeBookBtn);
+        bookDiv.appendChild(lineBreak);
+        bookContainer.appendChild(bookDiv);
+        removeBookBtn.addEventListener('click', () => {
+            removeBook(i, bookDiv);
+        });
     }
 };
 
 render();
+
+function removeBook(index, book) {
+    book.remove();
+    booksArr.splice(index,1);
+    localStorage.setItem('books', JSON.stringify(booksArr));
+}
 
 const add = (title, author) => {
     const book = new Book(title, author)
@@ -44,3 +61,6 @@ const add = (title, author) => {
 addBookBtn.addEventListener('click', () => {
     add(titleInput.value, authorInput.value);
 });
+
+
+
